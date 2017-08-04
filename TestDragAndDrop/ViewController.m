880863ce7371e18a63b13c71177d2b7289c9f420
毛广清 <gqmao@corp.netease.com>
2017-08-04 16:03:00 +0800

    git add test drag and drop

--- TestDragAndDrop/ViewController.m
+++ TestDragAndDrop/ViewController.m
@@ -8,18 +8,71 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UIDragInteractionDelegate,UIDropInteractionDelegate>
+{
+    NSMutableArray<UIDragItem *> *sessions;
+    UIImageView *dragView ;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIDragInteraction *dragAction = [[UIDragInteraction alloc] initWithDelegate:self];
+    [self.view addInteraction:dragAction];
+    
+    
+    UIDropInteraction *dropAction = [[UIDropInteraction alloc] initWithDelegate:self];
+    [self.view addInteraction:dropAction];
+    
+    dragView.interactions = @[dragAction,dropAction];
+
+}
+
+- (NSArray<UIDragItem *> *)dragInteraction:(UIDragInteraction *)interaction itemsForBeginningSession:(id<UIDragSession>)session{
+    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithObject:@"hello world"];
+    return @[itemProvider];
 }
 
 
+- (BOOL)dropInteraction:(UIDropInteraction *)interaction canHandleSession:(id<UIDropSession>)session{
+    if (session.localDragSession != nil) { //ignore drag session started within app
+        return false;
+    }
+    
+    BOOL canHandle = false;
+    canHandle = [session canLoadObjectsOfClass:[UIImage class]];
+    return canHandle;
+    
+    
+}
+- (void)dropInteraction:(UIDropInteraction *)interaction sessionDidEnter:(id<UIDropSession>)session{
+    
+}
+- (void)dropInteraction:(UIDropInteraction *)interaction performDrop:(id<UIDropSession>)session{
+    [session loadObjectsOfClass:[UIImage class] completion:^(NSArray<__kindof id<NSItemProviderReading>> * _Nonnull objects) {
+        for (id object in objects) {
+            UIImage* image = (UIImage*)object;
+            if (image) {
+                //handle image
+                dragView.image = image;
+                [sessions addObject:[[UIDragItem alloc] initWithItemProvider:[[NSItemProvider alloc] initWithObject:image]]];
+            }
+        }
+    }];
+    
+}
+
+
+- (UIDropProposal *)dropInteraction:(UIDropInteraction *)interaction sessionDidUpdate:(id<UIDropSession>)session{
+    return [[UIDropProposal alloc] initWithDropOperation:UIDropOperationCopy];
+}
+- (nullable UITargetedDragPreview *)dropInteraction:(UIDropInteraction *)interaction previewForDroppingItem:(UIDragItem *)item withDefault:(UITargetedDragPreview *)defaultPreview{
+    
+    return nil;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
